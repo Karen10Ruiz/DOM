6fc25cf26095834ee5e7e01bcb7580f5d53579e1
Karen10Ruiz <163039934+Karen10Ruiz@users.noreply.github.com>
2024-11-27 12:52:25 -0600

    Asiganamos y eliminamos estilos con JS

--- 02-Estilos/script.js
+++ 02-Estilos/script.js
@@ -52,4 +52,75 @@ console.log(titulo.style)
 
 */
 
-console.log("Valores en línea. Color:" + titulo.style.color + " - Fondo: " + titulo.style.backgroundColor)
+console.log("Valores en línea. Color:" + titulo.style.color + " - Fondo: " + titulo.style.backgroundColor);
+
+/*
+
+    Método de propiedad 
+
+    Asignar el valor de la propiedad de estilo en el elemento seleccionado.
+
+    elemento.style.nombrePropiedad = "valor"
+
+    -Es la forma más directa y legible de asignar propiedades.
+    -Sólo admite asignar propiedades con formato camelCase.
+
+
+
+*/
+
+//let colorUsuario = promp("Escribe un color");
+
+titulo.style.color = "purple";
+titulo.style.backgroundColor = "yellow";
+
+console.log("Valores asignados con JS. Color:" + titulo.style.color + " - Fondo: " + titulo.style.backgroundColor);
+
+/*
+
+    Método setProperty()
+
+    Asignamos una propiedad de estilo al elemento seleccionado.
+
+    elemento.style.setProperty(nombrePropiedad, valor, important)
+
+    -Más flexible y poderoso.
+    -Las propiedades se escriben en formato kebab-case.
+    -Parámetro "important" es opcional.
+
+*/
+
+titulo.style.setProperty("color" , "darkblue");
+titulo.style.setProperty("background-Color" , "red" , "important");
+
+console.log("Valores asignados con setProperty. Color:" + titulo.style.color + " - Fondo: " + titulo.style.backgroundColor);
+
+/*
+
+    Eliminar valores de propiedades
+
+    Usamos el método de propiedad y le definimos una cadena de texto vacía.
+
+    elemento.style.nombrePropiedad = " "
+
+    Esto elimina los valores previamente definidos en JS y en los estilos en línea.
+
+*/
+
+//titulo.style.color = "";
+//titulo.style.backgroundColor = "";
+
+/*
+
+    Método removeProperty()
+
+    Ésto elimina la propiedad de estilo 
+
+    elemento.style.removeProperty(nombrePropiedad)
+
+    -Las propiedades se pasan en formato kebab-case
+
+*/
+
+titulo.style.removeProperty("color");
+titulo.style.removeProperty("background-color");
